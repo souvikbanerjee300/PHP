66636c52239373a6998beeff3def512341ff18bd
Souvik <souvikbanerjee241@gmail.com>
2023-08-03 09:36:37 +0530

    first day progress

---
+++ embeded_php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo("this is "); echo(" title section"); ?></title>
+</head>
+<body>
+    <?php
+      echo("this is the 1st embeded php program");
+    ?>
+</body>
+</html>
